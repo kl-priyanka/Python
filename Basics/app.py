f729f42ab7900c3410e9d86ac54e9687cecb810d
kl-priyanka <priyankz432@gmail.com>
2025-09-01 13:58:53 +0530

    01-09-2025 : Silly code

---
+++ Basics/app.py
@@ -0,0 +1,9 @@
+print("Kantimahanti Lakshmi Priyanka")
+
+#Python code is executed line by line from top to bottom
+print("This is a dog")
+print("o----")
+print(" ||||")
+
+print("*" * 10) #prints 10 times *
+# "*" * 10 is an expression. An expression is a piece of code that produces a value.
