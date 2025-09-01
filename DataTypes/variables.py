9f19f50ed352bcb44579a298fa48436ab5aeacbd
kl-priyanka <priyankz432@gmail.com>
2025-09-01 21:52:29 +0530

    datatypes

---
+++ DataTypes/variables.py
@@ -0,0 +1,14 @@
+price = 10 #integer
+
+#price is an identifier or variable
+
+#when interpreter executes this line, it allocates some memory, then it will store 10 in that memory
+#and it will label that memory with the name price
+
+price = 20 #reassigning a variable
+rating = 4.9 #float
+name = "Priyanka" #string
+is_published = False #boolean
+
+print(price)
+
