a77afed22edf3d11924f4c1a65c2b089b1e65e5f
kl-priyanka <priyankz432@gmail.com>
2025-09-21 13:47:42 +0530

    type_conversion

---
+++ DataTypes/type_conversion.py
@@ -0,0 +1,11 @@
+birth_year = input('Birth year: ')
+age = 2025-int(birth_year)
+print(age)
+
+#int() used to convert a value to integer
+#float() used to convert a value to float
+#bool() used to convert a value to boolean
+#str() used to convert a value to string
+
+print(type(birth_year))
+print(type(age))
