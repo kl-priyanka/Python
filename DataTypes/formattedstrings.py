ed9a00a4d145603022aef51f2245a2c34e9611e8
kl-priyanka <priyankz432@gmail.com>
2025-09-22 09:39:58 +0530

    formattedStrings done

---
+++ DataTypes/formattedstrings.py
@@ -0,0 +1,9 @@
+first = "Priyanka"
+last = "Kantimahanti"
+
+message = first + " [" + last + "] is a coder"
+
+print(message)
+
+message2 = f"{first} [{last}] is a coder" #this is a formatted string
+print(message2)
